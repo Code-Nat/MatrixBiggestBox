ce7bde5049e6f8cb623b32e1b87358315013d053
Code-Nat <44806486+Code-Nat@users.noreply.github.com>
2018-12-06 11:59:46 +0200

    Initial commit

---
+++ src/BiggestBox.java
@@ -0,0 +1,17 @@
+
+public class BiggestBox 
+{
+	public static int biggestRect (int[][] a)
+	{
+		a.
+		return a[1][1];
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a[][];
+		
+	}
+
+}
